1217e1e949eac9f93e65d873c42229356080c9a7
Manuel Eusebio de Paz Carmona <manuel.depaz@beeva.com>
2015-07-14 10:15:12 +0200

    Cleaning listImages

--- libs/apis.js
+++ libs/apis.js
@@ -3,13 +3,10 @@ module.exports = {
   status: '/v1/_ping', 
   // alias for fetcing status
   ping: '/v1/_ping',
+  listImages: '/v1/search',
   searchImage: {
     method: 'get',
     url: '/v1/search?q={{token}}'
-  },
-  listImages: {
-    method: 'get',
-    url: '/v1/search'
   },
   getImageLayer: {
     method: 'get',
